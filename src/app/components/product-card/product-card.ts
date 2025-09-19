be3704e404ab59be615e8f37de8e52bec0eade9c
Sarda, Mukund (Cognizant) <mukund.sarda@cognizant.com>
2025-09-19 14:53:13 +0530

    product card, product list created

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './product-card.html',
-  styleUrl: './product-card.css'
+  styleUrls: ['./product-card.css']
 })
-export class ProductCard {
+export class ProductCardComponent {
+  @Input() product!: Product;
+  @Output() addToCart = new EventEmitter<void>();
 
+  onAddToCart(): void {
+    this.addToCart.emit();
+  }
 }
